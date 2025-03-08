8d3e88853bd07d4ad714ad366b1790103803ca35
Jayrhon Bayla <98704058+JRON24@users.noreply.github.com>
2025-03-09 00:59:10 +0800

    Update ProfilePicture.jsx

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -4,7 +4,7 @@ function ProfilePicture({ imageUrl }) {
   return (
     <div>
       <img
-        src={imageUrl || "https://via.placeholder.com/150"}
+        src={imageUrl || "https://th.bing.com/th/id/OIP.XHy01a9BgJcKRzm9eJQpeAHaFH?rs=1&pid=ImgDetMain"}
         alt="Profile"
         className="rounded-circle"
         style={{ width: "150px", height: "150px" }}
